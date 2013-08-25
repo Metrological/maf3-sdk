3c41de126a9d73f8dc1fc4308fc25f02429dc6d3
albertd <albert@dahan.nl>
2013-08-26 01:11:49 +0200

    maf-sdk: adding all plugin types

--- plugin/generic.js
+++ plugin/generic.js
@@ -2,5 +2,5 @@ if (!plugins.storage) {
 	plugins.storage = new AutoStorage();
 }
 if (plugins.players.length === 0) {
-	plugins.players.push(new HTML5Player('tv'));
+	plugins.players.push(new HTML5Player(Player.TV), new HTML5Player(Player.PIP), new HTML5Player(Player.MUSIC, Player.type.AUDIO), new HTML5Player(Player.FX, Player.type.AUDIO));
 }
