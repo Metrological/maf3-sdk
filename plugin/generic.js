a5fc77e20973bf157f0f1526bb352231b96d22c4
albertd <albert@dahan.nl>
2013-08-26 02:11:33 +0200

    maf-sdk: cleaning up

--- plugin/generic.js
+++
@@ -1,6 +0,0 @@
-if (!plugins.storage) {
-	plugins.storage = new AutoStorage();
-}
-if (plugins.players.length === 0) {
-	plugins.players.push(new HTML5Player(Player.TV), new HTML5Player(Player.PIP), new HTML5Player(Player.MUSIC, Player.type.AUDIO), new HTML5Player(Player.FX, Player.type.AUDIO));
-}
