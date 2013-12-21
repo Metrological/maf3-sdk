78194a4c53e741a8d6bfe1ffba554586d8465108
albertd <albert@dahan.nl>
2013-12-21 13:16:28 +0100

    wyplay: remove key mappings

--- plugin/wyplay.js
+++ plugin/wyplay.js
@@ -1,10 +1,10 @@
 wms.AV.coopLevel = wms.AV.coopLevelValue.Exclusive;
-
+/*
 KeyMap.defineKeys(KeyMap.NORMAL, {
 	46: 'back',
 	19: 'stop'
 }, true);
-
+*/
 var releasePlayer = function () {
 	try {
 		wms.AV.coopLevel = wms.AV.coopLevelValue.Priority;
