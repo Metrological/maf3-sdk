f06665757ee1fb1f9339754024fd75ca9abb8369
albertd <albert@dahan.nl>
2014-08-26 10:58:44 +0200

    maf3-sdk: adding a clean app template

---
+++ apps/com.metrological.app.EmptyTemplate/Contents/Javascript/Views/MyView.js
@@ -0,0 +1,19 @@
+// Create a class and extended it from the MAF.system.SidebarView
+var MyView = new MAF.Class({
+	ClassName: 'MyView',
+
+	Extends: MAF.system.SidebarView,
+
+	// Create your view template
+	createView: function () {
+		// Reference to the current view
+		var view = this;
+	},
+
+	// After create view and when returning to the view
+	// the update view is called
+	updateView: function () {
+		// Data from previous view 
+		var view = this;
+	}
+});
