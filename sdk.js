0e674a8d54ca28d134d985804c20230dd7e8e165
albertd <albert@dahan.nl>
2014-08-28 01:18:41 +0200

    maf3-sdk: use express and make http port configurable

--- sdk.js
+++ sdk.js
@@ -1,4 +1,13 @@
-var connect = require('connect');
-connect.createServer(
-	connect.static(__dirname)
-).listen(8080);
+var express = require('express'),
+	morgan  = require('morgan'),
+	compression = require('compression'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	port = process.env.NODE_PORT || 8080;
+
+app.use(morgan('dev'));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+app.disable('x-powered-by');
+app.listen(port);
