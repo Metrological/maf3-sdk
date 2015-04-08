72355545a3e7afcf29a3b9bca388c62737f03c4f
Albert Dahan <albert@dahan.nl>
2015-04-08 16:35:46 +0200

    maf3-sdk: add horizon ui 2.1.8

---
+++ apps/com.metrological.ui.Horizon/Contents/Javascript/init.js
@@ -0,0 +1,24 @@
+Theme.set({
+	BaseFocus: {
+		styles: {
+			backgroundColor: 'rgba(3,138,224,.5)'
+		}
+	}
+});
+
+var TOS = widget.getSetting('tosVersion') || 1;
+
+include('Javascript/Core/Horizon.js');
+include('Javascript/Core/ApplicationManager.js');
+include('Javascript/Views/Apps.js');
+
+Facebook.init('183031261739046');
+Twitter.init('requiredATM');
+
+MAF.application.init({
+	views: [
+		{ id: 'view-Apps', viewClass: AppsView }
+	],
+	defaultViewId: 'view-Apps',
+	settingsViewId: null
+});
