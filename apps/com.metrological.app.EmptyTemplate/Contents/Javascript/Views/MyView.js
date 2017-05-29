834475ff6e53bcb1757ae52d6a3d1d954ad5051a
Jasper Vermeulen <jaspervermeulen1@gmail.com>
2017-05-29 12:54:35 +0200

    update EmptyTemplate

--- apps/com.metrological.app.EmptyTemplate/Contents/Javascript/Views/MyView.js
+++ apps/com.metrological.app.EmptyTemplate/Contents/Javascript/Views/MyView.js
@@ -1,19 +1,17 @@
-// Create a class and extended it from the MAF.system.SidebarView
-var MyView = new MAF.Class({
+// Create a new View class and extended it from the MAF.system.SidebarView
+var MyView = new MAF.Class( {
 	ClassName: 'MyView',
 
 	Extends: MAF.system.SidebarView,
 
 	// Create your view template
-	createView: function () {
-		// Reference to the current view
-		var view = this;
-	},
+	createView: function() {
+    // Reference to the current view
+    var view = this;
+  },
 
-	// After create view and when returning to the view
-	// the update view is called
-	updateView: function () {
-		// Reference to the current view
-		var view = this;
-	}
-});
+	// After create view and when returning to the view the update view is called
+	updateView: function() {
+    var view = this;
+  }
+} );
--- apps/com.metrological.app.EmptyTemplate/Contents/Javascript/init.js
+++ apps/com.metrological.app.EmptyTemplate/Contents/Javascript/init.js
@@ -1,12 +1,12 @@
 // Include your views
-include('Javascript/Views/MyView.js');
+include( 'Javascript/Views/MyView.js' );
 
 // Init application with view config
-MAF.application.init({
+MAF.application.init( {
 	views: [
-		{ id: 'view-MyView', viewClass: MyView },
-		{ id: 'view-About', viewClass: MAF.views.AboutBox } // Use standard About view
+		{ id: 'MyView', viewClass: MyView },
+		{ id: 'About', viewClass: MAF.views.AboutBox } // Use standard About view
 	],
-	defaultViewId: 'view-MyView', // Declare what view to be loaded when opening the app
-	settingsViewId: 'view-About' // Declare what view is opened when a used loads the settings
-});
+	defaultViewId: 'MyView', // Declare what view to be loaded when opening the app
+	settingsViewId: 'About' // Declare what view is opened when a used loads the settings
+} );
